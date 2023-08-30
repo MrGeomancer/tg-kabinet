7c378296f9bc6085b94b6d88a76e5a69eaba075f
Geomancer <vityatyniadec@gmail.com>
2023-08-30 17:30:20 +0300

    теперь кнопки 2 в ряд

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 import config
 
 # Включаем логирование, чтобы не пропустить важные сообщения
@@ -13,12 +14,20 @@ dp = Dispatcher()
 
 # Хэндлер на команду /start
 @dp.message(Command("start"))
-async def cmd_start(message: types.Message):
-    await message.answer("Hello!")
+async def reply_builder(message: types.Message):
+    builder = ReplyKeyboardBuilder()
+    builder.add(types.KeyboardButton(text='Мой кабинет'))
+    builder.add(types.KeyboardButton(text='Мои кейсы'))
+    builder.add(types.KeyboardButton(text='Посмотреть на валюты'))
+    builder.add(types.KeyboardButton(text='Посмотреть на кейсы'))
+    builder.adjust(2)
+    await message.answer(
+        "Какие котлетки?:",
+        reply_markup=builder.as_markup(resize_keyboard=True),)
 
-@dp.message(Command("dice"))
-async def cmd_dice(message: types.Message):
-    await message.answer_dice("🎲")
+# @dp.message(Command("dice"))
+# async def cmd_dice(message: types.Message):
+#     await message.answer_dice("🎲")
 async def main():
     await dp.start_polling(bot)
 
