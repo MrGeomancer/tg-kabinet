745d5f760c633abd8deeb53c067325f12c895c5e
MrGeomancer <101983215+MrGeomancer@users.noreply.github.com>
2023-12-19 23:04:47 +0500

    починил малясь, хз как раньше работало

--- main.py
+++ main.py
@@ -9,33 +9,34 @@ import config
 logging.basicConfig(level=logging.INFO)
 # Объект бота
 bot = Bot(config.bot_token)
-class ClientState(StatesGroup):
-    Sravnenie_gr_1 = State()
-    Sravnenie_price_1 = State()
-    Sravnenie_gr_2 = State()
-    Sravnenie_price_2 = State()
+dp = Dispatcher()
+# class ClientState(StatesGroup):
+#     Sravnenie_gr_1 = State()
+#     Sravnenie_price_1 = State()
+#     Sravnenie_gr_2 = State()
+#     Sravnenie_price_2 = State()
 
 # Хэндлер на команду /start
 @dp.message(Command("start"))
 async def reply_builder(message: types.Message):
     builder = ReplyKeyboardBuilder()
-    builder.add(types.KeyboardButton(text='Посмотреть на кейсы'))
-    builder.add(types.KeyboardButton(text='Посмотреть на валюты'))
-    builder.add(types.KeyboardButton(text='Мой кабинет'))
-    builder.add(types.KeyboardButton(text='Сравнить цены'))
+    builder.add(types.KeyboardButton(text='📈 Посмотреть на кейсы'))
+    builder.add(types.KeyboardButton(text='📈 Посмотреть на валюты'))
+    builder.add(types.KeyboardButton(text='🛒 Сравнить цены'))
+    builder.add(types.KeyboardButton(text='👨‍🏫 Мой кабинет'))
+
     builder.adjust(2)
     await message.answer(
         "Какие котлетки?:",
         reply_markup=builder.as_markup(resize_keyboard=True))
         
-@dp.message(Text("Сравнить цены"))
-async def 
+# @dp.message(Text("Сравнить цены"))
+# async def
 
 # @dp.message(Command("dice"))
 # async def cmd_dice(message: types.Message):
 #     await message.answer_dice("🎲")
 async def main():
-    dp = Dispatcher()
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
