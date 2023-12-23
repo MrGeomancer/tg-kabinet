deddf3ef367962a6191b449daca6bad1417fcc9b
MrGeomancer <101983215+MrGeomancer@users.noreply.github.com>
2023-12-23 13:59:59 +0500

    начинаем двигаться в сторону парсинга

---
+++ parsing.py
@@ -0,0 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
+
+cookies = {
+    'sessionid': '3f508f3dc960c4ec077ddab2',
+    'timezoneOffset': '18000,0',
+    'Steam_Language': 'russian',
+    'browserid': '2895299699782351373',
+    'webTradeEligibility': '%7B%22allowed%22%3A1%2C%22allowed_at_time%22%3A0%2C%22steamguard_required_days%22%3A15%2C%22new_device_cooldown_days%22%3A0%2C%22time_checked%22%3A1687720529%7D',
+    '_ga': 'GA1.2.1815743433.1687720529',
+    'steamCountry': 'RU%7Cf786fce45ccb3e777a33903acb1c1f31',
+    'recentlyVisitedAppHubs': '1328660%2C2532550%2C1170970%2C814380%2C1222140%2C761890%2C304930%2C1517290%2C615530%2C239140%2C2073850%2C1272080%2C872200%2C730%2C255710%2C508440%2C931690%2C1117090%2C1174180%2C632470',
+    'steamLoginSecure': '76561198165608999%7C%7CeyAidHlwIjogIkpXVCIsICJhbGciOiAiRWREU0EiIH0.eyAiaXNzIjogInI6MEQwNF8yMkQyOEYxM18zOTQyOCIsICJzdWIiOiAiNzY1NjExOTgxNjU2MDg5OTkiLCAiYXVkIjogWyAid2ViIiBdLCAiZXhwIjogMTcwMzMxODQ1NiwgIm5iZiI6IDE2OTQ1OTE5NzIsICJpYXQiOiAxNzAzMjMxOTcyLCAianRpIjogIjBERDZfMjNBQkNEQ0NfNDQ4NzciLCAib2F0IjogMTY4OTI2MTE5NiwgInJ0X2V4cCI6IDE3MDcyNzQ2MTAsICJwZXIiOiAwLCAiaXBfc3ViamVjdCI6ICIxODguMjI2LjExMC4xMCIsICJpcF9jb25maXJtZXIiOiAiMTg4LjIyNi4xMTAuMTAiIH0.vLxT_tUURURYcIpWyTiKnJKv_WWdGSA7VPLHToJ6cPhflM3BDSumBYpgZWPfnnSX0oT27dWNfTRKa87qDm7_AQ',
+    'strInventoryLastContext': '252490_2',
+}
+
+headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Cache-Control': 'max-age=0',
+    'Connection': 'keep-alive',
+    'Referer': 'https://steamcommunity.com/market/',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'same-origin',
+    'Sec-Fetch-User': '?1',
+    'Upgrade-Insecure-Requests': '1',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.0.0',
+    'sec-ch-ua': '"Not_A Brand";v="8", "Chromium";v="120", "Microsoft Edge";v="120"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+}
+
+response = requests.get('https://steamcommunity.com/market/listings/730/Dreams%20%26%20Nightmares%20Case', cookies=cookies, headers=headers)
+print(response)
+bs = BeautifulSoup(response.text,"lxml")
+# print(bs)
+token = bs.find("div", 'responsive_page_template_content')
+# print (token)
+print(str(token)[str(token).index('Market_LoadOrderSpread')+24:str(token).index('PollOnUserActionAfterInterval')-23])
