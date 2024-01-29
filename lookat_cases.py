2af97d98d0d00dee2ca41611afb952a3c7b55b4e
MrGeomancer <vityatyniadec@gmail.com>
2024-01-29 18:02:47 +0500

    обработка пустой базы данных

--- lookat_cases.py
+++ lookat_cases.py
@@ -14,13 +14,16 @@ router = Router()
 @router.message(F.text == "📈 Посмотреть на кейсы")
 async def kabinet_main_page(message: Message, state: FSMContext):
     await state.clear()
-    data_list = await parsing.get_pricec(message.from_user.id)
-    ttext = ''
-    for item in data_list.values():
-        nowpricedigit = float(item['nowprice'][:-5].replace(',','.'))
-        if nowpricedigit>item['price']: ttext+='🟢'
-        else: ttext+='🟥'
-        ttext+=f'<b>{item['name']}</b> сейчас можно продать за <b>{item['nowprice']}</b>\n'
+    data_list = await parsing.get_prices(message.from_user.id)
+    if data_list == '':
+        ttext = 'База данных пустая и смотреть тут не на что 👀‼️. Сначала зайди в кабинет и добавить кейсы.'
+    else:
+        ttext = ''
+        for item in data_list.values():
+            nowpricedigit = float(item['nowprice'][:-5].replace(',','.'))
+            if nowpricedigit>item['price']: ttext+='🟢'
+            else: ttext+='🟥'
+            ttext+=f'<b>{item['name']}</b> сейчас можно продать за <b>{item['nowprice']}</b>\n'
     await message.answer(text=ttext, parse_mode=ParseMode.HTML)
     # print(textt)
     # await message.answer(text=textt)
